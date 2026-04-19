a48ea2997e929b2587410645ccbd944ac52c737b
Jason Li <jason@hanming.li>
2026-04-19 22:44:50 +0800

    简化显示

--- frontend/src/components/AirportHint.vue
+++ frontend/src/components/AirportHint.vue
@@ -13,52 +13,38 @@
       <p class="card-text"></p>
       <table class="table table-sm spread">
         <tbody>
-          <tr>
-            <th>Field Elevation</th>
-            <td>{{ store.转换高度(airport.properties.aixm_field_elevation) }}</td>
-          </tr>
-          <tr>
-            <th>Reference Temperature</th>
-            <td>{{ store.转换温度(airport.properties.aixm_reference_temperature_in_celcius) }}</td>
-          </tr>
-          <DisplayList
-            v-for="([a, b, c], idx) in items"
-            :key="idx"
-            :title="a"
-            :horizontal="b"
-            :items="c"
-          />
+          <DisplayList v-for="([a, c], idx) in items" :key="idx" :title="a" :items="c" />
         </tbody>
       </table>
     </div>
   </div>
 </template>
+
 <script setup lang="ts">
 import { useGreatCircleMapStore } from '@/stores/GreatCircleMap'
-import type { 机场 } from '@/stores/types'
+import { type 机场 } from '@/stores/types'
 import { computed, type ComputedRef } from 'vue'
 import DisplayList from './DisplayList.vue'
 
 const store = useGreatCircleMapStore()
 const props = defineProps<{ airport: 机场 }>()
 
-const items: ComputedRef<[string, boolean, string[]][]> = computed(() => [
-  ['Magnetic Variation', false, props.airport.properties.aixm_magnetic_variation_display],
+const items: ComputedRef<[string, string[]][]> = computed(() => [
+  ['Field Elevation', [store.转换高度(props.airport.properties.aixm_field_elevation)]],
   [
-    'Annotations',
-    false,
-    props.airport.properties.aixm_annotations.split(/[:,]/).map((x) => x.trim()),
+    'Reference Temperature',
+    [store.转换温度(props.airport.properties.aixm_reference_temperature_in_celcius)],
   ],
+  ['Magnetic Variation', props.airport.properties.aixm_magnetic_variation_display],
+  ['Annotations', props.airport.properties.aixm_annotations.split(/[:,]/).map((x) => x.trim())],
   [
     'Version',
-    false,
     [
-      `Version ${props.airport.properties.aixm_sequence_number}.${props.airport.properties.aixm_correction_number}`,
+      `${props.airport.properties.aixm_sequence_number}.${props.airport.properties.aixm_correction_number}`,
     ],
   ],
   [
     'Validity',
-    false,
     [
       `Since ${props.airport.properties.information_valid_since}`,
       `Until ${props.airport.properties.information_valid_until}`,
@@ -66,6 +52,7 @@ const items: ComputedRef<[string, boolean, string[]][]> = computed(() => [
   ],
 ])
 </script>
+
 <style scoped>
 .spread td {
   text-align: end;
--- frontend/src/components/DisplayList.vue
+++ frontend/src/components/DisplayList.vue
@@ -1,21 +1,12 @@
 <template>
   <tr v-if="items.length > 0">
     <th>{{ title }}</th>
-    <td style="text-align: end" v-if="horizontal">
-      <div class="row">
-        <div class="col" v-for="(value, index) in items" :key="index">{{ value }}</div>
-      </div>
-    </td>
-    <td style="text-align: end" v-else>
+    <td style="text-align: end">
       <div v-for="(value, index) in items" :key="index">{{ value }}</div>
     </td>
   </tr>
 </template>
 
 <script setup lang="ts">
-defineProps<{
-  title: string
-  items: string[]
-  horizontal?: boolean
-}>()
+defineProps<{ title: string; items: string[] }>()
 </script>
